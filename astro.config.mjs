609eac93920093ecb46c6763ff1c39729982c799
Paul Scanlon <pauliescanlon@gmail.com>
2023-06-12 16:55:48 -0400

    test: manually add site path to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import { loadEnv } from 'vite';
-const { PUBLIC_SEO_URL, PUBLIC_BASE_PATH } = loadEnv(import.meta.env.MODE, process.cwd(), '');
+// import { loadEnv } from 'vite';
+// const { PUBLIC_SEO_URL, PUBLIC_BASE_PATH } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: PUBLIC_SEO_URL,
+  site: 'https://www.cockroachlabs.com/resources/resource-cloud-portability',
   base: '/resources/resource-cloud-portability',
   integrations: [
     tailwind({
