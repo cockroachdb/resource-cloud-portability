684bf5331111fa039efddf8531e21b75c6af7377
Paul Scanlon <pauliescanlon@gmail.com>
2023-06-13 09:28:38 -0400

    test: use env vars in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-// import { loadEnv } from 'vite';
-// const { PUBLIC_SEO_URL, PUBLIC_BASE_PATH } = loadEnv(import.meta.env.MODE, process.cwd(), '');
+import { loadEnv } from 'vite';
+const { PUBLIC_SEO_URL, PUBLIC_BASE_PATH } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://www.cockroachlabs.com/resources/resource-cloud-portability/',
-  base: '/resources/resource-cloud-portability',
+  site: PUBLIC_SEO_URL,
+  base: PUBLIC_BASE_PATH,
   integrations: [
     tailwind({
       config: {
